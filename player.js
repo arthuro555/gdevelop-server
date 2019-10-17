36948eebe407118213e59e06988d2e7937be3eff
arthuro555 <arthur.pacaud@hotmail.fr>
2019-10-17 19:43:26 +0200

    Added Token system to prevent other players to modify data from other

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 let crypto = require('crypto');
 const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
 var jwt = require('jsonwebtoken');
 const settings = require("./confighandler.js").config;
 
@@ -66,13 +67,20 @@ class player {
         }
         return null; //not found
     };
-    addObject(object){
+    addObject(token, object){
+        if(!this.verifyToken(token)){
+            return false;
+        }
         if (!this.online){
             throw "Trying to access data from a non-online player!";
         }
         this.data.push(object)
+        return true
     };
-    removeObject(name = null, uuid = null){
+    removeObject(token, name = null, uuid = null){
+        if(!this.verifyToken(token)){
+            return false;
+        }
         if (!this.online){
             throw "Trying to access data from a non-online player!";
         }
@@ -94,15 +102,51 @@ class player {
         }
         return false;
     };
-    getToken(){
-        return this.token;
+    verifyToken(token){
+        try {
+            var exists = false;
+            for(var t in this._token){
+                if(t[0] === token){
+                    exists = true;
+                    var tuuid = t[1];
+                }
+            }
+            if(exists === false){return false}
+            var data = jwt.verify(token, settings["SECRET"]);
+            return data["username"] === this.username && data["password"] === this._password && data["tokenUUID"] === tuuid;
+        } catch (e){
+            return false;
+        }
     };
-    login(password){
-        return crypto.createHash('sha256').update(password).digest('hex') === this._password;
+    removeToken(token){
+        if(this.verifyToken(token)){
+            var i;
+            for (i = 0; i < this._token.length; i += 1) {
+                if (this._token[i][0] === token) {
+                    this._token.splice(i, 0);
+                    return true;
+                }
+            }
+            return false; //not found
+        }
     }
-    logout(){
-        this.data = Array();
-        this.online = false;
+    login(password){
+        password = crypto.createHash('sha256').update(password).digest('hex');
+        if(password === this._password){
+            var tuuid = uuidv4();
+            var token = jwt.sign({"username": this.username, "password": password, "tokenUUID":tuuid}, settings["SECRET"]);
+            this._token.push([token,tuuid]);
+            return token;
+        }
+    }
+    logout(token){
+        if(this.verifyToken(token)) {
+            this.removeToken(token);
+            this.data = Array();  //Clear all data
+            this.online = false;
+            return true;
+        }
+        return false;
     }
     getObjects(){
         return this.data;
